815079b94dc1acb6d4f962fd5466d43acdaaa1d6
Ravindra <Ravindra@DESKTOP-ID1I68B>
2022-05-05 19:10:07 +0530

    Commit5

---
+++ Pojo porgram/src/com/pojo/Cat.java
@@ -0,0 +1,25 @@
+package com.pojo;
+
+import java.io.Serializable;
+
+public class Cat implements Serializable{
+	
+	private int age ;
+	private String name;
+	
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
---
+++ Pojo porgram/src/com/pojo/PojoExample.java
@@ -0,0 +1,12 @@
+package com.pojo;
+
+public class PojoExample {
+
+	public static void main(String[] args) {
+		
+		Cat myCat = new Cat();
+
+		
+	}
+
+}
