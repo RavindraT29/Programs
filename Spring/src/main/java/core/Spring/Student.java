054b187850f2962d7fbab2e22921a01d852d9bce
Ravindra <Ravindra@DESKTOP-ID1I68B>
2022-05-05 19:19:55 +0530

    Spring core program

---
+++ Spring/src/main/java/core/Spring/Student.java
@@ -0,0 +1,47 @@
+package core.Spring;
+
+public class Student {
+	
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	
+	
+	// Geater and Seater method
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(String studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
+	// create constructor
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+	public Student() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+}
